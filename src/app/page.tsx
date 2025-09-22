fae7ecefe78c5a6bc143f4fb29d8e3cff52ca62b
peregrinoo <gabriel.ribeiro07@aluno.ifce.edu.br>
2025-09-22 16:15:14 -0300

    fix: '/' at the end of page. And removing the recommendations part.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import LandingPagePresentation from "@/modules/Landing-page/steps/landing-page.presentation";
 import LandingPageAboutMe from "@/modules/Landing-page/steps/landing-page.about-me";
 import LandingPageProjects from "@/modules/Landing-page/steps/landing-page.projects";
-import LandingPageRecommendations from "@/modules/Landing-page/steps/landing-page.recommendations";
 import LandingPageContact from "@/modules/Landing-page/steps/landing-page.contact";
 
 export default function Home() {
@@ -10,8 +9,7 @@ export default function Home() {
             <LandingPagePresentation />
             <LandingPageAboutMe />
             <LandingPageProjects />
-            <LandingPageRecommendations/>
-            <LandingPageContact/>\
+            <LandingPageContact/>
         </>
     )
 }
